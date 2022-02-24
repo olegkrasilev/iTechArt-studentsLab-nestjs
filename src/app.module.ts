93310c2767dac0b6917f95e525be61f3f2928fdc
olegkrasilev <65223358+olegkrasilev@users.noreply.github.com>
2022-02-24 09:19:07 +0300

    fix(app): db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,7 @@ import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [
-    AuthModule,
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "bob",
-      password: "admin",
-      database: "nest-js",
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-  ],
+  imports: [AuthModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
